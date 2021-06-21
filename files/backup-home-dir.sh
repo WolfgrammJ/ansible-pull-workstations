1565e8faa9d718f999858ee0fbc40f6a2107c0f3
Jens Wolfgramm <Jens.Wolfgramm@damantec.de>
2021-06-21 10:46:42 +0200

    Added vault to cronjob & Added backup-script for home-dir

---
+++ files/backup-home-dir.sh
@@ -0,0 +1,25 @@
+# !/usr/bin/env bash
+# This script backs up my documents
+# filename: backup-documents-to-nas.sh
+
+# Parameters
+sourcefolder=$HOME/
+targetfolder=/mnt/NASDMT003/linux_home_backup/$(hostname)/
+
+logdirectory=$HOME/logs/backup
+logfilepath="$logdirectory/backup-home-dir.log"
+
+# Prequisites
+# Create log directory if it does not exist
+if [ ! -d "$logdirectory" ]; then
+	mkdir -p "$logdirectory"
+fi
+
+# Create target directory if it does not exist
+if [ ! -d "$targetfolder" ]; then
+        mkdir -p "$targetfolder"
+fi
+
+
+# Start Sync
+rsync -itru --delete --exclude ".cache/*" --log-file=$logfilepath $sourcefolder $targetfolder
