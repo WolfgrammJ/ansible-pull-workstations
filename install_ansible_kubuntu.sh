a411b030c90b3d59a4a9661338145b02b6f35b9b
WolfgrammJ <81314305+WolfgrammJ@users.noreply.github.com>
2023-10-31 05:34:25 +0100

    Update install_ansible_kubuntu.sh

--- install_ansible_kubuntu.sh
+++ install_ansible_kubuntu.sh
@@ -2,3 +2,4 @@ sudo apt install pipx
 pipx install --include-deps ansible
 pipx inject --include-apps ansible argcomplete
 pipx ensurepath
+sudo ansible-pull -U https://github.com/WolfgrammJ/ansible-pull.git ubuntu.yml
