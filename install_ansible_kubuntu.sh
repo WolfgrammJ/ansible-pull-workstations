d3cb574513aa566771a3c693527cdc42022a851f
Jens Wolfgramm <Jens.Wolfgramm@damantec.de>
2023-10-31 07:49:34 +0100

    changes

--- install_ansible_kubuntu.sh
+++ install_ansible_kubuntu.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt update
+#sudo apt-add-repository ppa:ansible/ansible
+#sudo apt update
 sudo apt install ansible
 sudo apt install python3-argcomplete
 
