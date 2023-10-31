eaa0029a9cd1557cb42231c636903ad20003d007
Jens Wolfgramm <Jens.Wolfgramm@damantec.de>
2023-10-31 07:31:29 +0100

    Changes

--- install_ansible_kubuntu.sh
+++ install_ansible_kubuntu.sh
@@ -4,7 +4,5 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible
 sudo apt install python3-argcomplete
-sudo activate-global-python-argcomplete3
-sudo ansible galaxy collection install
 
 sudo ansible-pull -U https://github.com/WolfgrammJ/ansible-pull.git ubuntu.yml
