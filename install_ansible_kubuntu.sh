672f23a1292ebd13e5c83f481b181d364f24468e
WolfgrammJ <81314305+WolfgrammJ@users.noreply.github.com>
2024-05-17 16:02:42 +0200

    changes

--- install_ansible_kubuntu.sh
+++ install_ansible_kubuntu.sh
@@ -4,5 +4,6 @@ sudo apt install software-properties-common -y
 #sudo apt update
 sudo apt install ansible -y
 sudo apt install python3-argcomplete -y
+sudo ansible-galaxy collection install community.general
 
 ansible-pull -U https://github.com/WolfgrammJ/ansible-pull-workstation.git workstation.yml --checkout main
